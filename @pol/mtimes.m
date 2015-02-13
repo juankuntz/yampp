18d28b48e49ed77f60801722505d908a03699328
Juan Kuntz <jk208@ic.ac.uk>
2015-02-13 09:37:32 +0000

    Overloaded @pol/mtimes.m so it supports inner products between a pol object and a seq (or a sdpvar) object.

--- @pol/mtimes.m
+++ @pol/mtimes.m
@@ -5,6 +5,34 @@ function g = mtimes(p,q)
 
 % Juan Kuntz 08/02/2015
 
+j = 1;
+
+% Case 1: Inner product between sdpvar (or seq) object and a pol object.
+
+
+if isa(q,'sdpvar') || isa(q,'seq')
+    
+    
+    if (min(size(q)) ~= 1 && isa(q,'sdpvar')) || ~isscalar(p) || (~isscalar(q) && isa(q,'seq'))
+        disp('Error: Inner product between sdpvar and pol classes only implemented for scalars.');
+        return
+    end
+    
+    % Compute inner product.
+    g = 0;
+    np = numel(p.coef(1,:));
+    for i = 1:numel(q)
+        if j <= np && p.coef(2,j) == i
+            g = g + p.coef(1,j)*q(i);
+            j = j + 1;
+        end
+    end
+    return
+end
+
+% Case 2: Normal scalar or matrix multiplcation between pols and/or a pol
+% and a real number.
+
 [np,mp] = size(p); [nq,mq] = size(q);
 
 if np == 1 && mp == 1 % Scalar product
