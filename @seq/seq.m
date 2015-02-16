d045640f96c7fa81e4fbf8353f6c65f1ed68e547
Juan Kuntz <jk208@ic.ac.uk>
2015-02-16 19:34:23 +0000

    Added functionality that allows the seq class constructor to convert a vector of doubles into a sequence object with those coefficients.

--- @seq/seq.m
+++ @seq/seq.m
@@ -1,7 +1,8 @@
 classdef seq
     
-    %SEQ Summary of this function goes here
-    %Detailed explanation goes here
+    % Missing comments.
+    
+    % Juan Kuntz, 16/02/2015, last modified 16/02/2015.
 
     properties
         coef = []; % 2 x length(coef) array. The first row contains a nonzero coefficient of the polynomial and the corresponding entry of the second row contains the grlex (in sum(var.ncomp) variables) rank.
@@ -15,11 +16,25 @@ classdef seq
             if isempty(varargin)
                 disp('Error: you must specify the dimension of the underlying space');
                 return
-            elseif nargin == 2
-                obj.ord = varargin{2};
-                obj.choose = ncktab(varargin{1}+varargin{2});
             end
-            obj.dim = varargin{1};
+            
+            if nargin >= 1
+                obj.dim = varargin{1};
+            end
+            
+            if nargin >= 2
+                obj.ord = varargin{2};
+                obj.choose = ncktab(obj.dim+varargin{2});
+            end
+            
+            if nargin == 3  % Convert vector of doubles into sequence object.
+                if numel(varargin{3}) ~= nchoosek(obj.dim+obj.ord,obj.ord)
+                    disp('Error the length of the sequence does not match up with the specified dimension and order.')
+                    return
+                end
+                obj.coef = [varargin{3}(:)';1:nchoosek(obj.dim+obj.ord,obj.ord)];
+            end
+            
         end
     end
 end
