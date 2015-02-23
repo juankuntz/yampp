9b0e6f1958acf1b115691e6d78cb96f26d32e490
Juan Kuntz <jk208@ic.ac.uk>
2015-02-23 19:31:33 +0000

    The extracting of the dual residues for relaxations D and DD now work properly.

--- @scp/solvescp.m
+++ @scp/solvescp.m
@@ -42,10 +42,13 @@ for i = 1:numel(cp.obj)
     if strcmp(cp.minmax,'min')
         temp = optimize(cp.ycons,cp.yobj(i),cp.ops); % Compute solution.
         cp.sol{end+1}.minmax = 'min';
+        
+        cp.sol{end}.dval = -value(dual(cp.ycons(1))); % Not sure why we need the minus sign here...
     elseif strcmp(cp.minmax,'max')
         temp = optimize(cp.ycons,-cp.yobj(i),cp.ops);
         cp.sol{end+1}.minmax = 'max';
         
+        cp.sol{end}.dval = value(dual(cp.ycons(1))); 
     end
 
         % Store solution.
@@ -58,8 +61,51 @@ for i = 1:numel(cp.obj)
         
         cp.sol{end}.pval = value(cp.yobj(i));
         cp.sol{end}.ppoint = seq(n,2*d,value(cp.yvar));
-        [cp.sol{end}.pres,~] = check(cp.ycons);
+        
+        
+        % Store primal residues in a formated table.
+        
+        [temp2,~] = check(cp.ycons);
+        
+        cp.sol{end}.pres{1,2} = [];
+        
+        l = 0;
+        if ~isempty(cp.mass)
+            cp.sol{end}.pres{1,1} = 'Mass constraint.';
+            cp.sol{end}.pres{1,2} = temp2(1);
+            l = l + 1;
+        end
+        
+        if ~isempty(cp.seqeqcon)
+            if l == 0
+                cp.sol{end}.pres{1,1} = 'Equality constraints.';
+                cp.sol{end}.pres{1,2} = temp2(1);
+            else
+                cp.sol{end}.pres{2,1} = 'Equality constraints.';
+                cp.sol{end}.pres{2,2} = temp2(2);
+            end
+            l = l + 1;
+        end
+        
+        if l == 0
+            cp.sol{end}.pres{1,1} = 'Cone constraints.';
+            cp.sol{end}.pres{1,2} = temp2(1);
+        else
+            cp.sol{end}.pres{end+1,1} = 'Cone constraints.';
+            cp.sol{end}.pres{end,2} = temp2(l+1);
+        end
+        l = l + 1;
+        
+       for j = 1:numel(cp.supcon)
+            cp.sol{end}.pres{end,2} = [cp.sol{end}.pres{end,2};temp2(l+j)];
+       end
+        
+        % Store yalmip output info.
+        
         cp.sol{end}.info = temp; clear temp;
+        
+        % If problem is unbounded or infeasible store the appropiate +-inf
+        % into the primal value.
         
         if cp.sol{end}.info.problem == 1 
             if strcmp(cp.minmax,'min')
@@ -75,8 +121,8 @@ for i = 1:numel(cp.obj)
             end
         end
 
-        cp.sol{end}.dval = -value(dual(cp.ycons(1))); % Not sure why we need the minus sign here...
-
+        % Compute and store the dual residues in a formated table.
+        
         if ~isempty(cp.mass) && ~isempty(cp.seqeqcon)
             t = dual(cp.ycons(1)); % Dual of the mass constraint.
             t(2:numel(cp.seqeqcon)+1,1) = dual(cp.ycons(2)); % Dual of the equality constraints
@@ -86,73 +132,96 @@ for i = 1:numel(cp.obj)
             t = dual(cp.ycons(1)); % Dual of the equality constraints
         end
         
-        cp.sol{end}.dres = cp.F'*t; 
+        temp = cp.F'*t; 
         clear t;
 
             
+        if strcmp(cp.minmax,'min')
+            temp = temp + cp.b{i};
+        else
+            temp = temp - cp.b{i};
+        end
+
+        % Onto the support constraints.
+
+        cp.sol{end}.dres{1,1} = 'Equality constraints';
+            
     switch cp.reltype
-        case 'D' 
+        case {'D','DD'}
             
-            % This is strange, but it seems we need to switch the sign here
-            % depending on whether it's an LP, SOCP, or SDP...
-            
-            cp.sol{end}.dres = cp.sol{end}.dres + cp.b{i};
-            
-            % Onto the support constraints.
-            
-            for j = 1:numel(cp.supcon)+1        % Fish out the correct duals.
-                if ~isempty(cp.mass) && ~isempty(cp.seqeqcon) 
-                    X{j} = dual(cp.ycons(2+j));
-                elseif ~isempty(cp.mass) || ~isempty(cp.seqeqcon)
+            for j = 1:numel(cp.supcon)+1
+                if isempty(cp.mass) && isempty(cp.seqeqcon)
+                    X{j} = dual(cp.ycons(j));
+                elseif isempty(cp.mass) || isempty(cp.seqeqcon)
                     X{j} = dual(cp.ycons(1+j));
                 else
-                    X{j} = dual(cp.ycons(j));
+                    X{j} = dual(cp.ycons(2+j));
+                end
+            end
+        
+            for j = 1:numel(cp.supcon)+1
+                temp = temp + cp.A{j}*X{j};
+            end
+            
+            cp.sol{end}.dres{1,2} = temp;   clear temp
+            
+%             cp.sol{end}.dres{2,1} = 'Cone constraints';
+%             for j = 1:numel(cp.supcon)+1
+%                 cp.sol{end}.dres{2,2} = [cp.sol{end}.dres{2,2};min(X{j})];
+%             end
+
+        case 'SDD'
+            
+            if isempty(cp.mass) && isempty(cp.seqeqcon)
+                L = 0;
+            elseif isempty(cp.mass) || isempty(cp.seqeqcon)
+                L = 1;
+            else
+                L = 2;
+            end
+            
+            J = 1;
+            for j = 1:numel(cp.supcon)+1
+                X{j} = [];
+                A = cp.A{j};
+                for k = 1:numel(A(1,:))
+                    X{j} = [X{j};dual(cp.ycons(L+J))];
+                    J = J + 1;
                 end
             end
             
-            for j = 1:numel(cp.supcon)+1
-                cp.sol{end}.dres = cp.sol{end}.dres + cp.A{j}*X{j};
-            end
-            
-            for j = 1:numel(cp.supcon)+1
-                cp.sol{end}.dres = [cp.sol{end}.dres;X{j}];
-            end
-            
-        case 'DD'
-            
-        case 'SDD'
-
         case 'FKW'
 
         case 'PSD'
             
-            % This is strange, but it seems we need to switch the sign here
-            % depending on whether it's an LP, SOCP, or SDP...
-            
-            if strcmp(cp.minmax,'min')
-                cp.sol{end}.dres = cp.sol{end}.dres + cp.b{i};
-            else
-                cp.sol{end}.dres = cp.sol{end}.dres - cp.b{i};
-            end
-            
-                for j = 1:numel(cp.supcon)+1
+            for j = 1:numel(cp.supcon)+1
+                if isempty(cp.mass) && isempty(cp.seqeqcon)
+                    X{j} = dual(cp.ycons(j));
+                elseif isempty(cp.mass) || isempty(cp.seqeqcon)
+                    X{j} = dual(cp.ycons(1+j));
+                else
                     X{j} = dual(cp.ycons(2+j));
                 end
-
-                for j = 1:numel(cp.supcon)+1
-                    temp = [];
-                    A = cp.A{j};
-                    for k = 1:numel(cp.A{j})
-                        temp = [temp;sum(sum(X{j}.*A{k}))];
-                    end
-                    clear A;
-
-                    cp.sol{end}.dres = cp.sol{end}.dres + temp;
+            end
+        
+            for j = 1:numel(cp.supcon)+1
+                temp2 = [];
+                A = cp.A{j};
+                for k = 1:numel(cp.A{j})
+                    temp2 = [temp2;sum(sum(X{j}.*A{k}))];
                 end
+                clear A;
 
-                for j = 1:numel(cp.supcon)+1
-                    cp.sol{end}.dres = [cp.sol{end}.dres;min(eig(X{j}))];
-                end
+                temp = temp + temp2; clear temp2
+            end
+
+            cp.sol{end}.dres{1,2} = temp; clear temp
+            cp.sol{end}.dres{2,2} = [];
+            
+            for j = 1:numel(cp.supcon)+1
+                cp.sol{end}.dres{2,1} = 'Cone constraints';
+                cp.sol{end}.dres{2,2} = [cp.sol{end}.dres{2,2};min(eig(X{j}))];
+            end
     end
         
 end
