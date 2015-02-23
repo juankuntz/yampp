5dd2fdc4f4e65358a61041e5061b4ba5088ce835
Juan Kuntz <jk208@ic.ac.uk>
2015-02-23 16:31:12 +0000

    Modified /auxiliary/ncktab.m so that it safes the nck tables in a global variable, which it later calls in order to avoid unnecessary calls of nchoosek.m and thus speed up computations.

--- Auxiliary/ncktab.m
+++ Auxiliary/ncktab.m
@@ -1,15 +1,28 @@
 function out = ncktab(n)
 
 % Creates table out such that out(i,j) = i-1 choose j-1 for all i
-% <= n+1 and j <= i.
+% <= n+1 and j <= i. This table is stored for future use in the global
+% variable NCKTAB. If NCKTAB already exists and is at least as large as
+% (n+1)^2, then we just return out = NCKTAB. We do this to speed up
+% computations by avoiding unnecessary calls of nchoosek.
 
-% Juan Kuntz 08/02/2015
+% Juan Kuntz 08/02/2015, last edited 23/02/2015
 
-out = zeros(n);
+global NCKTAB
 
-for i = 1:n+1
-    for j = 1:i
-        out(i,j) = nchoosek(i-1,j-1);
+if isempty(NCKTAB) || NCKTAB.n < n
+    NCKTAB = [];
+    NCKTAB.tab = zeros(n);
+    NCKTAB.n = n;
+    for i = 1:n+1
+        for j = 1:i
+            NCKTAB.tab(i,j) = nchoosek(i-1,j-1);
+        end
     end
 end
+
+out = NCKTAB.tab;
+
 end
+
+
