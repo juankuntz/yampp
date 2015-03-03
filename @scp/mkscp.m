juankuntz/yampp

commit 5c8f0a0594bf3afb1e901860ec47ef4a443026e6

Sped up the code up slightly by initialising properly several variables (used to compute the duals for DD and SDD relaxations).

@scp/mkscp.m
@@ -144,7 +144,7 @@ tab = ncktab(n+2*d);
 % General moment constraint
 
 l = nchoosek(n+2*d,2*d);
-T = zeros(nchoosek(n+d,d),l);
+T = zeros(3*nchoosek(n+d,d),l);
 
 mon = zeros(cp.nvar,l);
 
@@ -160,7 +160,6 @@ end
 f = T*(1:l)';
 for i = 1:nchoosek(n+d,d)
     for j = 1:i-1
-        T = [T;zeros(2,l)];
         k = igrlext(mon(:,i)+mon(:,j),tab);
         T(end-1,f(i)) = 1; T(end-1,f(j)) = 1; T(end-1,k) = -2; % y_2a+y_2b - 2y_(a+b)>=0
         T(end,f(i)) = 1; T(end,f(j)) = 1; T(end,k) = 2; % y_2a+y_2b - 2y_(a+b)>=0

@scp/solvescp.m
@@ -181,20 +181,24 @@ for i = 1:numel(cp.obj)
                 L = 2;
             end
             
+            % Initialise SOC dual variables
+            
+            X{1} = zeros(3*nchoosek(n+d,d),1);
+            for j = 1:numel(cp.supcon)
+                X{j+1} = zeros(3*nchoosek(n+floor(d-cp.supcon(i).deg/2),floor(d-cp.supcon(i).deg/2),1));
+            end
+            
+            % Populate the SOC duals
+            
             J = 1;
             for j = 1:numel(cp.supcon)+1
-                X{j} = []; A{j} = [];
                 temp2 = cp.A{j};
                 for k = 1:numel(temp2)
-                    A{j} = [A{j},temp2{k}];
-                    X{j} = [X{j};dual(cp.ycons(L+J))];
+                    X{j}(1+3*(k-1):3*k,:) = dual(cp.ycons(L+J));
+                    temp = temp + temp2{k}*X{j}(1+3*(k-1):3*k,:);
                     J = J + 1;
                 end
                 clear temp2
-            end
-            
-            for j = 1:numel(cp.supcon)+1
-                temp = temp + A{j}*X{j};
             end
             
             cp.sol{end}.dres{1,2} = -max(abs(temp));    % Return Linfty norm of equality constraing violations

commit 7d50cabc6459d59c0fa68a93b5cba468ccabf35c

Fixed a bug introduced in commit 5c8f0a0594bf3afb1e901860ec47ef4a443026e6.

@scp/mkscp.m
@@ -144,7 +144,7 @@ tab = ncktab(n+2*d);
 % General moment constraint
 
 l = nchoosek(n+2*d,2*d);
-T = zeros(3*nchoosek(n+d,d),l);
+T = zeros(nchoosek(n+d,d)^2,l);
 
 mon = zeros(cp.nvar,l);
 
@@ -157,16 +157,18 @@ end
 
 % Now the off-diagonal constaints.
 
-f = T*(1:l)';
+C = nchoosek(n+d,d);
+f = T(1:nchoosek(n+d,d),:)*(1:l)';
 for i = 1:nchoosek(n+d,d)
     for j = 1:i-1
+        C = C + 2;
         k = igrlext(mon(:,i)+mon(:,j),tab);
-        T(end-1,f(i)) = 1; T(end-1,f(j)) = 1; T(end-1,k) = -2; % y_2a+y_2b - 2y_(a+b)>=0
-        T(end,f(i)) = 1; T(end,f(j)) = 1; T(end,k) = 2; % y_2a+y_2b - 2y_(a+b)>=0
+        T(C-1,f(i)) = 1; T(end-1,f(j)) = 1; T(end-1,k) = -2; % y_2a+y_2b - 2y_(a+b)>=0
+        T(C,f(i)) = 1; T(end,f(j)) = 1; T(end,k) = 2; % y_2a+y_2b - 2y_(a+b)>=0
     end
 end
 
-clear f l mon k
+clear f l mon k C
 
 cp.ycons = [cp.ycons,T*y >= 0];
 cp.A{1} = -T'; 