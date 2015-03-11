f01760aa7651f3539b3fd876f92f041340287216
Juan Kuntz <jk208@ic.ac.uk>
2015-03-11 19:14:41 +0000

    Fixed some bugs in subasgn.m and added overloaded horzcat.m and vertcat.m methods that update the variables and degree all of all polynomials involved.

---
+++ @pol/horzcat.m
@@ -0,0 +1,62 @@
+function g = horzcat(varargin)
+
+% Overloads horzcat so that the variables of the input matrices of
+% polynomials are updated, for example g = [p,q] returns the concatenation
+% of x and y, but the variables of all entries of p include both those p
+% and those of q.
+
+% Juan Kuntz, 10/03/2015, last edited 11/03/2015.
+
+% Remove empty polynomials
+
+k = 0;
+for i = 1:nargin
+    if ~isempty(varargin{i})
+        k = k + 1;
+        temp{k} = varargin{i};
+    end
+end
+
+for i = 1:k
+    [n(i),m(i)] = size(temp{i});
+end
+
+for i = 2:k
+    if n(i-1) ~= n(i)
+        error('To concatenate two matrices of polynomials horizontally, each matrix must have the same number of rows');
+    end
+end
+
+% Initialise g and adding the variables of temp{1} into it.
+
+g(n(1),sum(m)) = pol;
+
+% Populate g
+
+for i = 1:k
+    
+    p = temp{i};
+    
+    % Make sure that g and p have the same variables.
+    
+    if ~strcmp(g(1,1).var.symb,p(1,1).var.symb)
+        [gnotp,pnotg] = varcomp(g(1,1),p(1,1));  
+        for j = 1:n(1)
+            for k = 1:sum(m)
+                if ~isempty(pnotg.symb)
+                    g(j,k).var = pnotg; 
+                end
+            end
+            for k = 1:m(i)
+                if ~isempty(gnotp.symb)
+                    p(j,k).var = gnotp;
+                end
+            end
+        end
+    end
+
+    g(:,sum(m(1:i-1))+1:sum(m(1:i))) = p; % Add in the next matrix.
+    
+    clear p gnotp pnotg
+end
+end
--- @pol/subsasgn.m
+++ @pol/subsasgn.m
@@ -3,7 +3,8 @@ function out = subsasgn(p,s,q)
 % Overloaded subasgn so that it functions as usual, but if we are assigning
 % to a matrix, that is using '()', the function makes sure that all of the
 % entries of the assigned matrix are written in all relevant independent
-% variables.
+% variables. Only matrices supported, no higher dimensional arrays. Also,
+% it updates the degrees.
 
 % For example,
 %
@@ -29,6 +30,10 @@ switch s.type
     
     case '()'
         
+        if numel(s.subs)>2
+            error('Only matrix indexing permitted for polynomials, no higher dimensional arrays allowed. For example, X(3,4) = x(1), is fine, but X(3,4,1) = x(1) is not.');
+        end
+        
         % If q and p are both polynomials, then make sure they have the
         % same variables.
             
@@ -43,6 +48,7 @@ switch s.type
                 end
             end
             
+            
             if ~isempty(qnotp.symb)
                 for i = 1:numel(p(:,1))
                     for j = 1:numel(p(1,:))
@@ -50,15 +56,44 @@ switch s.type
                     end
                 end
             end
+            
+            % Update the degree.
+            
+            DEG = max(p(1,1).deg,q(1,1).deg);
+
+            for i = 1:numel(p(:,1))
+                for j = 1:numel(p(1,:))
+                    p(i,j).deg = DEG;
+                    q(i,j).deg = DEG;
+                end
+            end
+
 
         end
 
-        % If p is empty, we need to initialise it as a polynomial in order
-        % to use the buildin subsasgn below.
+        % If p is empty, we need to initialise it as a polynomial and make
+        % sure all its entries are written in the same variables (those of
+        % q). Also load the degree of q into that of p.
         
         if isempty(p)
-           p = pol; 
-           p.var = q(1,1).var;
+            
+            clear p
+            
+            n = s.subs{1};
+            m = 1;
+            if exist('s.subs{2}')
+                m = s.subs{2};
+            end
+            
+            p = pol(zeros([n,m]));
+            if ~isempty(q.var.symb)     % If p has variables, load them into q.
+                for i = 1:n
+                    for j = 1:m
+                        p(i,j).var = q.var;
+                        p(i,j).deg = q.deg; % Update degree.
+                    end
+                end
+            end
         end
         
         % If q is a double convert it into a polynomial in the variables of
@@ -68,9 +103,12 @@ switch s.type
             temp = pol(q); 
             clear q;
             q = temp;
-            for i = 1:numel(q(:,1))
-                for j = 1:numel(q(1,:))
-                    q(i,j).var = p.var; 
+            if ~isempty(p(1,1).var.symb)
+                for i = 1:numel(q(:,1))
+                    for j = 1:numel(q(1,:))
+                        q(i,j).var = p.var; 
+                        q(i,j).deg = p.deg; % Update degree.
+                    end
                 end
             end
         end
