6ae1bb6b43702d93992404e9a3161a1a4a41d21a
Juan Kuntz <jk208@ic.ac.uk>
2015-03-11 17:43:07 +0000

    Added cleanvars.m which can be used to remove symbols that no longer appear in the monomials of a polynomials. The idea is to use it to resolve issues #3 and #4.

---
+++ @pol/monvectors.m
@@ -0,0 +1,18 @@
+function out = monvectors(p)
+
+% Returns an array containing the multiindexes of the monomials with
+% nonzero coefficients in p, as column vectors, and in the order in which
+% the ranks of the monomials are stored in p.coef.
+
+% Juan Kuntz, 11/03/2015.
+
+TAB = ncktab(p.nvar+p.deg);
+l = numel(p.coef(1,:));
+
+out = zeros(p.nvar,l);
+
+for i = 1:l
+    out(:,i) = grlext(p.nvar,p.coef(2,i),TAB);
+end
+
+end
